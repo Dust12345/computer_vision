dab88ffed695a58eb379dbc145dc7d0fd86d0b5f
k-kroemmelbein <k.kroemmelbein+git@gmail.com>
2017-05-24 12:28:56 +0200

    Some documentation

--- Navigation/MovementHistory.cs
+++ Navigation/MovementHistory.cs
@@ -14,16 +14,26 @@ namespace Frame.VrAibo.Navigation
             _movementHistory = new Stack<MovementStep>();
         }
 
+        /// <summary>
+        /// Inserts a new movement step to the history
+        /// </summary>
+        /// <param name="step">The step to be inserted</param>
         public void push(MovementStep step)
         {
             _movementHistory.Push(step);
         }
 
+        /// <summary>
+        /// Removes and returns the movement step that was last inserted
+        /// </summary>
         public MovementStep pop()
         {
             return _movementHistory.Pop();
         }
 
+        /// <summary>
+        /// Clears the entire movement history
+        /// </summary>
         public void clear()
         {
             _movementHistory.Clear();
